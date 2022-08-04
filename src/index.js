8ce2c69a0721f1e8d775bb5a04ef5678749ab2a3
jimin <moana16@naver.com>
2022-08-04 20:20:09 +0900

    react 생활코딩2022 실습

--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App /> 
   </React.StrictMode>
 );
+
+// App 태그는 UI전체를 의미함
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
